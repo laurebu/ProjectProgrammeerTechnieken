df87c832fd9105308004e8662f957ba2e281e042
Lander <lander.buysse@gmail.com>
2019-11-05 13:20:37 +0100

    al de rest

---
+++ workspace/test/Debug/source/test.d
@@ -0,0 +1,15 @@
+source/test.o source/test.d: ../source/test.c ../device/MK64F12.h \
+ ../CMSIS/core_cm4.h ../CMSIS/cmsis_version.h ../CMSIS/cmsis_compiler.h \
+ ../CMSIS/cmsis_gcc.h ../device/system_MK64F12.h
+
+../device/MK64F12.h:
+
+../CMSIS/core_cm4.h:
+
+../CMSIS/cmsis_version.h:
+
+../CMSIS/cmsis_compiler.h:
+
+../CMSIS/cmsis_gcc.h:
+
+../device/system_MK64F12.h:
